6becf09b0620260c323c5091971b432be269f9f8
Gael12321 <142756857+Gael12321@users.noreply.github.com>
2023-10-10 18:41:34 -0700

    Update regex.rkt

--- tarea-05/regex.rkt
+++ tarea-05/regex.rkt
@@ -1,11 +1,11 @@
 #lang racket
 (define open-paren-regex #rx"\\(")
 (define close-paren-regex #rx"\\)")
-(define define-regex #rx"^define$")
-(define sum-regex #rx"^\\+$") 
-(define mult-regex #rx"^\\*$")
-(define identifier-regex #px"^[x-z][x-z\\d]*")
-(define number-regex #px"^[+-]?[\\d][\\d]*$")
+(define define-regex #rx"define")
+(define sum-regex #rx"\\+") 
+(define mult-regex #rx"\\*")
+(define identifier-regex #px"[x-z][x-z\\d]*")
+(define number-regex #px"[+-]?[\\d][\\d]*")
 
 (provide open-paren-regex
          close-paren-regex
